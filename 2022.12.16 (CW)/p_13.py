5de50088b99e6b9a4f4eac227d5163632390feea
Sagiman <sasha@sagiman.ru>
2022-12-16 12:08:11 +0700

    2022.12.16 (CW)

---
+++ 2022.12.16 (CW)/p_13.py
@@ -0,0 +1,28 @@
+from random import randint
+
+
+r = 6
+
+
+m = []
+
+
+for i in range(r):
+    m.append(list())
+    for j in range(r):
+        m[i].append(randint(-100, 200))
+
+
+print("Матрица А:")
+for i in m:
+    print(*i)
+
+
+print("\n" + "Элементы главной диагонали")
+for i in range(r):
+    print(m[i][i], end=" ")
+
+
+print("\n" + "\n" + "Элементы побочной диагонали")
+for i in range(r):
+    print(m[r - 1 - i][i], end=" ")
