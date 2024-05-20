a5222f36df4cb33342e77141ded86465093ec9dc
Sagiman <sasha@sagiman.ru>
2024-05-20 18:15:21 +0700

    27_dosrok

--- SESC NSU/2024.05.17 (HW)/27_dosrok.py
+++ SESC NSU/2024.05.17 (HW)/27_dosrok.py
@@ -1,15 +1,35 @@
-with open("27_dosrok_0.txt") as file:
-    lines = file.read().split("\n")
-    n, k = map(int, lines[0].split())
-    pre_data = lines[1:]
+# Ответ к файлу A – 129086
+# Ответ к файлу B – 15233987675003
 
-road = [0] * k
+with open("27_dosrok_A.txt") as file:
+    lines = file.read().split("\n")
+
+n, k = map(int, lines[0].split())
+pre_data = lines[1:]
+
+size = 11
+data = [0] * k
 for line in pre_data:
     temp = list(map(int, line.split()))
-    road[temp[0] - 1] = temp[1]
+    data[temp[0]] = (temp[1] + (size - 1)) // size
 
-size = 3
-fst_b = list(map(int, pre_data[0]))[0]
+s = 0
+total = sum(data)
+s_right = sum(data[1: (k // 2) + 1])
+s_left = total - s_right
 
-for v in range(n)
-    
+s += (k // 2) * data[(k // 2)]
+for i in range(1, k // 2):
+    s += i * (data[i] + data[k - i])
+
+min_s = s
+
+for i in range(1, k):
+    s = s - s_right + s_left
+    s_right = s_right - data[i] + data[(i + (k // 2)) % k]
+    s_left = total - s_right
+
+    if data[i] > 0 and s < min_s:
+        min_s = s
+
+print(min_s)
